c0714ea896dc3e7e69ef483906855c7b00306cd0
Graham Brown <graham.g.brown@gmail.com>
2021-10-21 00:38:11 -0400

    Delete src directory

--- src/scripts/phys/postSimulationCleanUp.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.system("rm ./workingData/hamiltonian/propagators/*")
-os.system("rm ./workingData/hamiltonian/propagatorsL/001/*")
-os.system("rm ./workingData/hamiltonian/propagatorsL/002/*")
